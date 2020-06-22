482930292dfa4028f37106daecb1aeb338eab6d8
lveeke <53772362+lveeke@users.noreply.github.com>
2020-06-22 21:02:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -27,11 +27,6 @@ Prettige dag.
 <h2>Anamnese</h2>
 <form method="post">
     <fieldset>
-        <label>Redenen voor het consult (in volgorde van belangrijkheid):<textarea id="TEXTAREA_ID" name="UNIEKE_NAAM" placeholder="PLACEHOLDER"></label>
-        <label>Doel van het consult (wat wilt u bereiken?):<textarea id="TEXTAREA_ID" name="UNIEKE_NAAM" placeholder="PLACEHOLDER"></label>
-    </fieldset>
-</form>
-    <fieldset>
         <label>Kleur</label><br>
         <input type="radio" id="blauw" name="kleur" value="blauw">
         <label for="blauw">Blauw</label><br>
@@ -43,6 +38,27 @@ Prettige dag.
         <label>Telefoonnummer<span class="req">*</span> <input type="tel" name="phone" placeholder="Telefoonnummer" required></label>
         <label>E-mailadres<span class="req">*</span> <input type="email" name="mail" placeholder="Email Address" required></label>
         <label>Favoriete kleur <input type="text" name="Favoriete kleur" placeholder="Favoriete kleur"></label>
+        <label>Redenen voor het consult (in volgorde van belangrijkheid):<textarea id="TEXTAREA_ID" name="UNIEKE_NAAM" placeholder="PLACEHOLDER"></label>
+        <label>Doel van het consult (wat wilt u bereiken?):<textarea id="TEXTAREA_ID" name="UNIEKE_NAAM" placeholder="PLACEHOLDER"></label>
+        <p>Andere artsen of therapeuten waar u onder behandeling bent</P>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="naam"></label>
+            <label>2. <input type="text" name="UNIEKE_NAAM" placeholder="praktijk"></label>
+            <label>3. <input type="text" name="UNIEKE_NAAM" placeholder="plaats"></label>
+        <p>Gebruikt u medicijnen? Zo ja, welke?</P>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="naam medicijn"></label>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="dosering"></label>
+        <p>Gebruikt u supplementen? Zo ja, welke?</P>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="naam supplement"></label>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="dosering"></label>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="merk"></label>
+        <p>Ziektegeschiedenis (aandoeningen, operaties, opnames etc.): </p>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="vul in"></label>
+            <label>1. <input type="text" name="UNIEKE_NAAM" placeholder="periode"></label>
+        <p>Specifieke gegevens</p>
+            <p>lengte:</p>
+            <label>NAAM_BOVEN_INVOERVELD<input type="text" name="UNIEKE_NAAM" placeholder="PLACEHOLDER"></label>
+            <p>kg</p>
+            
     </fieldset>
     <fieldset>
         <label><input type="checkbox" name="keep_me_informed"> Keep me informed!</label>
